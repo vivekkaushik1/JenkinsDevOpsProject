9c18491834a2c4f91ef135ca8b5bc3dfe9f8b1b3
sunilyerubandi9 <sunilyerubandi9@gmail.com>
2021-10-28 16:12:08 +0530

    Remove Lines as part of refactoring the method

--- src/main/java/com/sn/JenkinsDevOpsProject/App.java
+++ src/main/java/com/sn/JenkinsDevOpsProject/App.java
@@ -8,8 +8,6 @@ public class App {
     public static void main( String[] args )	{
     	
        System.out.println("STRY0010039: Story update");
-       System.out.println("STRY0010045: Story update");
-    
     }
 
 }
