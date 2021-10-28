1334a2b4ce765d1e75ad0c4cb70b0f6065ba205f
sunilyerubandi9 <sunilyerubandi9@gmail.com>
2021-10-28 16:19:27 +0530

    TEST: Adding New Integrations Tests

---
+++ src/test/java/com/sn/JenkinsDevOpsProject/TestDevOpsApp11.java
@@ -0,0 +1,36 @@
+package com.sn.TestSNOWPolarisUIRepo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestDevOpsApp10
+{
+    /**
+     * Rigorous Test :-)
+     */
+	
+	String devOpsTitle = "DevOps";
+	
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        //new change
+        //new change for Cascade Delete
+        //Another change for Cascade Delete
+        //Third Change
+        //Change for Cascade Delete, again
+        //Change For validation of PipelineUI Tests
+    	assertTrue( true );
+    }
+    
+    @Test
+    public void validateAddMethods()
+    {
+        assertTrue( true );
+        assertEquals(devOpsTitle.equals("DevOps"));
+    }
+}
